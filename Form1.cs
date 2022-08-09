ad89d314b2dacbb9ca21b82f8c24a809a377d501
luwitesfay <luwitesfay@gmail.com>
2022-08-09 11:57:53 -0700

    Activity 2

--- Form1.cs
+++ Form1.cs
@@ -9,18 +9,37 @@ namespace Activity_2
         //A butten click action that, on click it show the calculated value to the user
         private void button1_Click(object sender, EventArgs e)
         {
-            //if else statement - if statemnt that displayes a mssages when if the user doesn't neter any value
-            if (textBox1.Text == "")
+            //try mothod to allow user try a function and send an exception if they netr invalid value
+            try
             {
-                MessageBox.Show("Please enter a value");
+                //if else statement - if statemnt that displayes a mssages when if the user doesn't neter any value
+                if (textBox1.Text == "")
+                {
+                    MessageBox.Show("Please enter a value");
+                }
+                //else statement that takes a value string value from a user and conver it to a number.
+                // the system then takes the value and calculate it to diffrent value
+                else
+                {
+                    // Int cecuse to accept intige valuse form the user
+                    int c = Convert.ToInt32(textBox1.Text);
+
+                    // Double function to calculate the calculate the value of celcicius to Farenhite
+                    double f = ((c * 9) / 5) + 32;
+
+                    //Display the calulated value of farenhite
+                    textBox2.Text = f.ToString();
+
+
+                    //A doube defineing if there are more than 3 decimals to dispay only 3 or o=round it to three numbers
+                    double output = Math.Round(f, 3);
+                }
             }
-            //else statement that takes a value string value from a user and conver it to a number.
-            // the system then takes the value and calculate it to diffrent value
-            else
+
+            //Catch exception to catch the cepetion if user enetr invalid value
+            catch (Exception)
             {
-                float c = Convert.ToInt32(textBox1.Text);
-                float f = ((c * 9) / 5) + 32;
-                textBox2.Text = f.ToString();
+                MessageBox.Show("you have enterd invalid value, please enter a number");
             }
 
         }
